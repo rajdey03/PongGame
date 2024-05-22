066ffe03e3b876938c98f264b0d87d2b7fdbb744
Pritthiraj Dey <166027127+rajdey03@users.noreply.github.com>
2024-05-21 22:37:50 -0400

    Update PingPongGame.java

--- PingPongGame.java
+++ PingPongGame.java
@@ -2,17 +2,7 @@ package Ping_Pong;
 
 /*This is a ping pong game simulator. 
  * Writing this code taught me about Event Handling, Collision Detection and Graphics Rendering.
- * 
- * Event Handling: This code uses action event handling to respond to specifically keyboard inputs. For this game, it was only handling paddle controls.
- * 
- * Collision Detection: This code helped me understand collision logic in code. This game had a very basic logic behind collision, 
- * as the only thing constantly changing is the ball's velocity.
- * 
- * Graphics Rendering: Probably the coolest one, using Java's AWT and Swing libraries, I learned how to draw static shapes and texts on a created screen.
- * 
- * AWT: provides classes for GUIs
- * Swing: Additional components for creating GUIs, like frames, buttons, etc.
- * awt.event: Event handling for both AWT and Swing*/
+ */
 
 public class PingPongGame {
 
